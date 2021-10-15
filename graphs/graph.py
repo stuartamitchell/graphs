e599aff05003232bd3ff7d245d2ab6d294b0e50f
stuartamitchell <stuart.a.mitchell@gmail.com>
2021-10-16 06:31:37 +1100

    Update graph.py

--- graphs/graph.py
+++ graphs/graph.py
@@ -2,22 +2,67 @@ class Graph:
     '''
     A class to represent a graph - directed or undirected, weighted or unweighted
     '''
-    def __init__(self, adjacency_matrix=None, edges=None, directed=False, weighted=False):
+    def __init__(
+            self, adj_mat=None, edges=None, directed=False, weighted=False, sparse=True):
         self.directed = directed
         self.weighted = weighted
-        self.adjacency_matrix = adjacency_matrix
-        self.edges = edges
+        self.graph = None
 
-        self.graph = {}
+        if edges and sparse:
+            self.graph = self.__sparse_from_edges(edges)
 
-        if edges:
-            self.graph = self.__build_from_edges(edges)
+    def __sparse_from_edges(self, edges):
+        '''
+        Creates the graph as a dictionary of edges
 
-    def __build_from_edges(self,edges):
-        ''''''
-            
+        Parameters
+        ----------
+        edges : list
+            a list of the edges in the graph
 
-    def add(self, node1, node2):
-        ''''''
+        Returns
+        -------
+        dict
+            a dictionary keyed by the nodes of the graph whose
+            values are the node's neighbours
+        '''
 
-        
+        # get a list of all the nodes in the graph
+        first = [edge[0] for edge in edges]
+        second = [edge[1] for edge in edges]
+        nodes = sorted(set(first + second))
+
+        graph = {}
+
+        # create an empty dictionary of all the nodes
+        for node in nodes:
+            graph[node] = []
+
+        if self.weighted:
+            for node1, node2, weight in edges:
+                graph[node1].append((node2, weight))
+
+                if not self.directed:
+                    graph[node2].append((node1, weight))
+        else:
+            for node1, node2 in edges:
+                graph[node1].append(node2)
+
+                # if not a directed graph and the edge isn't a loop
+                if not self.directed and node1 != node2:
+                    graph[node2].append(node1)
+
+        return graph
+
+    def __dense_from_edges(self, edges):
+        '''
+        Creates the graph as an adjacency matrix
+        '''
+
+if __name__ == '__main__':
+    edges = [(1,2), (3,1), (2,2), (4,1)]
+    graph_edges = Graph(edges=edges)
+    graph_edges_directed = Graph(edges=edges, directed=True)
+    print(graph_edges.graph)
+    print(graph_edges_directed.graph)
+
